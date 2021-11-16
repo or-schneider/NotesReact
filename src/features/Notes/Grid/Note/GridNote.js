75a21555f604d47e31bdad88494bcd33bc62ac6b
Or S <orschneider@protonmail.com>
2021-11-16 02:38:53 +0200

    M4(WIP) Adds props null checks

--- src/features/Notes/Grid/Note/GridNote.js
+++ src/features/Notes/Grid/Note/GridNote.js
@@ -4,19 +4,29 @@ import formatDate from "./formatDate";
 
 class GridNote extends React.Component {
   render() {
+    if (!this.props.data) return null;
     const { createDate, title, content } = this.props.data;
     const formattedDate = formatDate(createDate);
 
     let titleElement = null;
     if (title.length > 0)
       titleElement = <h3 className={style.title}>{title}</h3>;
-
     return (
-      <div className={style.gridNote}>
+      <div
+        className={
+          style.gridNote + " " + (this.props.onClick ? style.button : "")
+        }
+        onClick={() => {
+          if (this.props.onClick) this.props.onClick(this.props.data);
+        }}
+      >
         <div className={style.header}>
           <div className={style.date}>{formattedDate}</div>
           <button
-            onClick={() => this.props.onDelete(this.props.data)}
+            onClick={(e) => {
+              e.stopPropagation();
+              if (this.props.onDelete) this.props.onDelete(this.props.data);
+            }}
             className={style.deleteButton}
           >
             &#10006;

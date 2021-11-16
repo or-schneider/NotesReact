3f17762c0873dcb933cc8d106bfe1e1f63c22073
Or S <orschneider@protonmail.com>
2021-11-16 02:48:29 +0200

    M4(F) Shows modal when clicking a note

--- src/features/Notes/Grid/NotesGrid.js
+++ src/features/Notes/Grid/NotesGrid.js
@@ -1,13 +1,19 @@
 import React from "react";
 import style from "./NotesGrid.module.css";
 import Note from "./Note/GridNote";
+
 class NotesGrid extends React.Component {
   render() {
     const { onDelete, notes } = this.props;
     const noteNodes = [];
     notes.forEach((note) => {
       noteNodes.push(
-        <Note onDelete={onDelete} key={note.createDate} data={note}></Note>
+        <Note
+          onDelete={onDelete}
+          onClick={(note) => this.props.onNoteClick(note)}
+          key={note.createDate}
+          data={note}
+        ></Note>
       );
     });
     return <div className={style.notesGrid}>{noteNodes}</div>;
--- src/features/Notes/NotesApp.js
+++ src/features/Notes/NotesApp.js
@@ -2,13 +2,18 @@ import React, { Component } from "react";
 import style from "./App.module.css";
 import NotesEditor from "features/Notes/Editor/NotesEditor";
 import NotesGrid from "features/Notes/Grid/NotesGrid";
+import NoteView from "features/Notes/Grid/Note/GridNote";
+import AppModal from "features/Notes/Modal/Modal";
+
 class NotesApp extends Component {
   constructor(props) {
     super(props);
-    this.state = { notes: new Map() };
+    this.state = { notes: new Map(), viewNote: null };
 
     this.addNote = this.addNote.bind(this);
     this.deleteNote = this.deleteNote.bind(this);
+    this.viewNote = this.viewNote.bind(this);
+    this.unViewNote = this.unViewNote.bind(this);
   }
   addNote(note) {
     this.setState(() => {
@@ -27,14 +32,27 @@ class NotesApp extends Component {
       return { notes };
     });
   }
+  viewNote(note) {
+    this.setState({ viewNote: note });
+  }
+  unViewNote() {
+    this.viewNote(null);
+  }
   render() {
     return (
       <div className={style.app}>
         <NotesEditor onNoteCreate={this.addNote}></NotesEditor>
         <NotesGrid
           onDelete={this.deleteNote}
+          onNoteClick={this.viewNote}
           notes={this.state.notes}
         ></NotesGrid>
+        <AppModal showModal={!!this.state.viewNote} close={this.unViewNote}>
+          <NoteView
+            onDelete={this.unViewNote}
+            data={this.state.viewNote}
+          ></NoteView>
+        </AppModal>
       </div>
     );
   }

d9ded4fb633b46ec806c2135b91f1c2f7b73d3f5
Or S <orschneider@protonmail.com>
2021-11-16 02:01:15 +0200

    M4(WIP) Adds react-modal package

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import NotesApp from "features/Notes/NotesApp";
+import Modal from "react-modal";
+Modal.setAppElement("#root");
 
 function App() {
   return <NotesApp></NotesApp>;
